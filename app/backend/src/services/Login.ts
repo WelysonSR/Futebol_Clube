270329e7d8567d1f5d362de8163f4e70b135d196
Welyson <welysonsr@gmail.com>
2022-11-23 13:31:16 -0300

    feat: não permita o acesso com uma senha inválida

--- app/backend/src/services/Login.ts
+++ app/backend/src/services/Login.ts
@@ -1,17 +1,19 @@
-// import * as bcrypt from 'bcryptjs';
 import UserModel from '../models/User';
 import { ILogin } from '../interfaces/ILogin';
 import { generateTokenJwt } from '../middlewares/jwt';
+import Bcrypt from '../assets/Bcrypt';
 
 export default class ServiceLogin {
   constructor(private model: UserModel = new UserModel()) {}
 
   public async login(user: ILogin) {
     const loginUser = await this.model.findOne(user.email);
-    // const sal = bcrypt.genSaltSync(6);
-    // const hash = bcrypt.hashSync(user.password, sal);
 
     if (!loginUser || loginUser === null) {
+      return { code: 401, message: 'Incorrect email or password' };
+    }
+
+    if (!Bcrypt.compare(loginUser.password, user.password)) {
       return { code: 401, message: 'Incorrect email or password' };
     }
 
