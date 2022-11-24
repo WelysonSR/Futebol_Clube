d368c6bcdc960e2f6f9e979fc557dc8d811bd3af
Welyson <welysonsr@gmail.com>
2022-11-24 15:03:37 -0300

    feat: tests

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -4,9 +4,10 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import App from '../app';
-import Example from '../database/models/ExampleModel';
+import UserModel from '../models/User';
 
 import { Response } from 'superagent';
+import TeamModel from '../models/Team';
 
 chai.use(chaiHttp);
 
@@ -14,34 +15,90 @@ const { app } = new App();
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+describe('Teste a rota Login', () => {
+  let chaiHttpResponse: Response;
 
-  // let chaiHttpResponse: Response;
+  before(async () => {
+    sinon
+      .stub(UserModel.prototype, "findOne")
+      .resolves({
+        id: 1,
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com',
+        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+      });
+  });
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
+  after(() => {
+    (UserModel.prototype.findOne as sinon.SinonStub).restore();
+  });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
+  it('caso de sucesso', async () => {
+    const result = await chai.request(app).post('/login').send({
+      email: 'admin@admin.com',
+      password: 'secret_admin'
+    });
+    expect(result.status).to.be.equal(200);  
+    expect(result.body).to.have.property('token');
+  });
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
+  it('caso de falha', async () => {
+    const result = await chai.request(app).post('/login').send({
+      email: 'teste@teste.com',
+      password: '123123',
+    });
 
-  //   expect(...)
-  // });
+    expect(result.status).to.equal(401);
+    expect(result.body).to.have.key('message');
+    expect(result.body.message).to.equal('Incorrect email or password');
+  });
+});
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+
+describe('Teste a rota Login Team', () => {
+  describe('Rota Get /team', () => {
+    before(async () => {
+      sinon
+        .stub(TeamModel.prototype, "findAll")
+        .resolves([
+          {
+            id:1,
+            teamName: 'Vitoria',
+          },
+          {
+            id:2,
+            teamName: 'Bahia',
+          }],
+        );
+    });
+
+    after(() => {
+      (TeamModel.prototype.findAll as sinon.SinonStub).restore();
+    });
+
+    it('Retorna os times com sucesso', async () => {
+      const result = await chai.request(app).get('/teams')
+      expect(result.status).to.be.equal(200);  
+      expect(result.body).to.be.an('array');
+    });  
+  })
+  describe('Rota Get /team error', () => {
+    before(async () => {
+      sinon
+        .stub(TeamModel.prototype, "findAll")
+        .resolves(null);
+    });
+
+    after(() => {
+      (TeamModel.prototype.findAll as sinon.SinonStub).restore();
+    });
+
+    it('Times com erro', async () => {
+      const result = await chai.request(app).get('/teams')
+      console.log(result.status, result.body)
+      expect(result.status).to.be.equal(404);
+      expect(result.body.message).to.be.equal('No teams found')
+    });  
   });
 });
